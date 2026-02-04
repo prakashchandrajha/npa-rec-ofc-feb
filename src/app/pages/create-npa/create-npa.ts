27d1e4c72cceb949ac3ace5b741a7f9ade1a9099
prakash <prakash@gmail.com>
2026-02-04 14:53:23 +0530

    q

--- src/app/pages/create-npa/create-npa.ts
+++ src/app/pages/create-npa/create-npa.ts
@@ -4,19 +4,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { NpaFormComponent } from '../../components/parent/npa-form/npa-form.component';
 
-interface Npa {
-  accountNumber: string;
-  accountType: string;
-  customerName: string;
-  customerId: string;
-  contactNumber: string;
-  npaDate: string;
-  npaCategory: string;
-  outstandingPrincipal: number;
-  interestDue: number;
-  totalAmountDue: number;
-  remarks: string;
-}
+
 
 @Component({
   selector: 'app-create-npa',
