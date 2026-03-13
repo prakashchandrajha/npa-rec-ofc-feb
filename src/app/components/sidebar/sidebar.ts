b63275cac74dc7e9d262641d6003f41d5ee14889
prakash <prakash@gmail.com>
2026-03-13 11:30:24 +0530

    q

--- src/app/components/sidebar/sidebar.ts
+++ src/app/components/sidebar/sidebar.ts
@@ -12,14 +12,20 @@ import { CommonModule } from '@angular/common';
 export class Sidebar implements OnInit {
   userInfo: UserInfo | null = null;
   isDivisionUser: boolean = false;
+  isNotAdmin: boolean = true;
+  isAdmin: boolean = false;
 
   constructor(private authService: AuthService) {}
 
   ngOnInit(): void {
     this.userInfo = this.authService.getUserInfo();
     this.isDivisionUser = this.userInfo?.userType === 'DIVISION';
+    this.isNotAdmin = this.userInfo?.userType !== 'ADMIN';
+    this.isAdmin = this.userInfo?.userType === 'ADMIN';
     console.log('Sidebar initialized, userInfo:', this.userInfo);
     console.log('isDivisionUser:', this.isDivisionUser);
+    console.log('isNotAdmin:', this.isNotAdmin);
+    console.log('isAdmin:', this.isAdmin);
   }
 
   logout(): void {
